5f29eea4195343cc29591ca6d44d5c0a82f4564c
DmitriySalko <dm.salko@gmail.com>
2015-10-01 03:33:13 +0300

    Fixed issues with Rails 4

--- lib/carrierwave/virus_free.rb
+++ lib/carrierwave/virus_free.rb
@@ -1,15 +1,9 @@
 require 'clam_scan'
 
-module CarrierWave
-  module VirusFree
-    class VirusFreeValidator < ActiveModel::EachValidator
-      def validate_each (record, attribute, value)
-        if value.present? && !ClamScan::Client.scan(location: value.url).safe?
-          record.errors.add(attribute, 'That file can not be accepted')
-        end
-      end
+class VirusFreeValidator < ActiveModel::EachValidator
+  def validate_each (record, attribute, value)
+    if value.present? && !ClamScan::Client.scan(location: value.url).safe?
+      record.errors.add(attribute, 'That file can not be accepted')
     end
   end
 end
-
-ActiveModel::Validations.include CarrierWave::VirusFree
